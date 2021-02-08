72fa5632c00415e33e0f8ffc590ae8eb52d449e5
miyoung-park <qkraldud1529@gmail.com>
2021-02-08 09:27:43 +0900

    2021.02.08 오전 9:30 미영 커밋

--- dangdang/src/com/dang/common/code/ConfigCode.java
+++ dangdang/src/com/dang/common/code/ConfigCode.java
@@ -5,8 +5,8 @@ public enum ConfigCode {
 	DOMAIN("http://localhost:8484"),
 	//EMAIL("suny10312@naver.com"),
 	EMAIL("qkraldud5050@naver.com"),
-	UPLOAD_PATH("C:\\semi\\dangWorkspace\\resources\\upload\\");
-	//UPLOAD_PATH("/Users/miyoung/Desktop/semi/CODE/resources/upload/");
+	//UPLOAD_PATH("C:\\semi\\dangWorkspace\\resources\\upload\\");
+	UPLOAD_PATH("/Users/miyoung/Desktop/semi/CODE/resources/upload/");
 	//업로드 해서 저장할 위치
 	
 	public String desc;
